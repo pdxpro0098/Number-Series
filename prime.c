204f6c05e08182e3e3dfae58d29b0ff7545ef827
PDX PRO <pdxpro0098@gmail.com>
2025-08-23 14:45:20 +0530

    prime number series

---
+++ prime.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int isPrime(int num)
+{
+    if (num <= 1)
+    {
+        return 0;
+    }
+    for (int i = 2; i * i <= num; i++)
+    {
+        if (num % i == 0)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main()
+{
+    for (int i = 0; i < 100; i++)
+    {
+        if (isPrime(i))
+        {
+            printf("%d ", i);
+        }
+    }
+
+    return 0;
+}
